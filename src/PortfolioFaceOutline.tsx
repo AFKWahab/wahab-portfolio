d0484e407ef1dd731a8712be815f20ccc4889bc9
Adam Wahab <adamfwahab@hotmail.com>
2025-09-23 21:10:06 +0200

    removed basic app.tsx code and added yarn aswell

---
+++ src/PortfolioFaceOutline.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const PortfolioFaceOutline = () => {
+    return (
+        <div>
+            <h2>Portfolio Face Outline</h2>
+        </div>
+    );
+};
+
+export default PortfolioFaceOutline;
